cc1454a2d1488cd00ec66b9558ea11abd41ec8cc
Kelsey Adams <kelsey.devon.adams@gmail.com>
2019-03-14 10:24:29 -0600

    Update scraper-dbconnect.py

--- scraper-dbconnect.py
+++ scraper-dbconnect.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 # pprint library is used to make the output look more pretty
 from pprint import pprint
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = MongoClient("mongodb://chive:WxOe3jPwS0oAPx1K@cluster0-ugvid.mongodb.net/")
+client = MongoClient("mongodb+srv://chive:WxOe3jPwS0oAPx1K@cluster0-ugvid.mongodb.net/test?retryWrites=true")
 db=client.admin
 # Issue the serverStatus command and print the results
 serverStatusResult=db.command("serverStatus")
